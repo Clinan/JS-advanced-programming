531de136386a6e900fdf35a8ef1ac6ebb4d11ab2
clinan <artergo163@gmail.com>
2019-03-16 15:15:58 +0800

    commit

--- chapter03/extra.js
+++
@@ -1,8 +0,0 @@
-function extra(){
-    // 额外补充
-
-    // 判断数据类型的的方式 typeof Object.prototype.toString.call([]) instanceof
-    typeof [] // object
-    Object.prototype.toString.call([]) // [object, Array]
-
-}
